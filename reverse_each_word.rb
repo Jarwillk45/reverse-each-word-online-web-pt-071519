0deb0724173beb3ce10aaa649c45dd5c66f4de30
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-08-07 02:50:51 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,5 @@
+def reverse_each_word(sentence)
+ sentence=[sentence1,sentence2]
+  return sentence.collect { |sentence1| sentence1.split.reverse!.join}
+  return sentence.collect { |sentence2| sentence2.split.reverse!.join}
+end  
