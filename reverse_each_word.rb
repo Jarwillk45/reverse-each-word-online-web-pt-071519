caa77f47dbe8c27e82c1cfc72c1ad982ce2fae82
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-08-07 02:34:14 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+def reverse_each_word(sentence)
+ sentence1="Hello there, and how are you?"
+ sentence2=
+ sentence= [sentence1, sentence2]
+  puts sentence.collect { |sentence| sentence.reverse!}
+end  
