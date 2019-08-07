778d0f5ca4cd12bef926d7365141847f37e9fe63
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-08-07 02:36:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence)
  sentence1="Hello there, and how are you?"
  sentence2="Hi again, just making sure it's reversed!"
  sentence= [sentence1, sentence2]
-  puts sentence.collect { |sentence| sentence.reverse!}
+  puts sentence.each { |sentence| sentence.reverse!}
 end  
